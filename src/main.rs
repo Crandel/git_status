3093bc468e906c0cc70e464dff4faad6dc65d883
Crandel <cradlemann@gmail.com>
2018-08-02 22:24:49 +0300

    Refactor to unified method and hash map for color

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 extern crate regex;
 
 use regex::Regex;
+use std::collections::HashMap;
 use std::env::args;
+use std::fmt::write;
 use std::process::Command;
 use std::str;
 use std::string::String;
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+enum Repo {
+    Branch,
+    Ahead,
+    Behind,
+    Unstaged,
+    Staged,
+}
 
 fn main() {
     let status_vec = Command::new("git")
@@ -19,6 +30,17 @@ fn main() {
     }
     let shell: &str = &shell_vec[1];
     if status.len() > 0 {
+        /*
+        git status -sb return all changes in repository
+
+          ## rrr-43...origin/rrr-43 [ahead 1][behind 3]
+          ?? <file 1> - new file
+           M <file 2> - modified file
+          D  <file 2> - deleted file
+
+        Using this output we could calculate changes and create repository status output for shell.
+         */
+
         let mut modified_unstaged = 0;
         let mut deleted_unstaged = 0;
         let mut untracked_unstaged = 0;
@@ -27,44 +49,48 @@ fn main() {
         let mut renamed_staged = 0;
         let mut new_staged = 0;
 
+        // First we split lines
         let vec_strings = status.split("\n").collect::<Vec<&str>>();
+        // First line give us a repo name and relation to remote server
         let repository_diff = match vec_strings.get(0) {
             Some(valid_str) => valid_str,
             _ => "",
         };
+        // Common regex for extracting numbers
         let number = Regex::new("[0-9+]").unwrap();
-        let ahead = Regex::new("ahead [0-9]+*").unwrap();
-        let branch_full_rex = Regex::new(r"## .+").unwrap();
-        let branch_rex = Regex::new(r"\w+").unwrap();
+
         // get ahead count
-        let ahead_str = match ahead.find(repository_diff) {
-            Some(ahead_str) => &repository_diff[ahead_str.start()..ahead_str.end()],
-            None => "",
-        };
-        let ahead_count = match number.find(ahead_str) {
-            Some(res) => &ahead_str[res.start()..res.end()],
-            None => "",
+        let ahead_reg = Regex::new("ahead [0-9]+*").unwrap();
+        let ahead_count = match ahead_reg.find(repository_diff) {
+            Some(ahead_str) => {
+                let ahead_str = &repository_diff[ahead_str.start()..ahead_str.end()];
+                match number.find(ahead_str) {
+                    Some(res) => &ahead_str[res.start()..res.end()],
+                    _ => "",
+                }
+            }
+            _ => "",
         };
 
         // get behind count
         let behind = Regex::new("behind [0-9]+*").unwrap();
-        let behind_str = match behind.find(repository_diff) {
-            Some(behind_str) => &repository_diff[behind_str.start()..behind_str.end()],
-            None => "",
-        };
-        let behind_count = match number.find(behind_str) {
-            Some(res) => &behind_str[res.start()..res.end()],
-            None => "",
-        };
-        // get branch
-        let branch = match branch_full_rex.find(repository_diff) {
-            Some(br) => {
-                let br_str = &repository_diff[br.start()..br.end()];
-                match branch_rex.find(br_str) {
-                    Some(branch_local) => &br_str[branch_local.start()..branch_local.end()],
+        let behind_count = match behind.find(repository_diff) {
+            Some(behind_str) => {
+                let behind_str = &repository_diff[behind_str.start()..behind_str.end()];
+                match number.find(behind_str) {
+                    Some(res) => &behind_str[res.start()..res.end()],
                     _ => "",
                 }
             }
+            _ => "",
+        };
+
+        // get branch
+        let branch_vec = &repository_diff[3..repository_diff.len() - 1]
+            .split('.')
+            .collect::<Vec<&str>>();
+        let branch = match branch_vec.get(0) {
+            Some(local_name) => local_name,
             _ => "",
         };
 
@@ -89,7 +115,7 @@ fn main() {
             }
         }
 
-        let mut branch_final = String::from(branch);
+        let mut branch_final = String::from("");
         let mut staged_counts = String::from("");
         let mut unstaged_counts = String::from("");
         if modified_unstaged > 0 {
@@ -115,35 +141,83 @@ fn main() {
             staged_counts.push_str(&format!("+{}", new_staged))
         }
 
-        match shell {
-            "zsh" => {
-                branch_final = format!("%F{{cyan}}{}%f", branch_final);
+        let mut bash_parser = HashMap::new();
+        bash_parser.insert(Repo::Branch, ("${CYAN}", "$NORMAL"));
+        bash_parser.insert(Repo::Ahead, ("${LIGHT_GREEN}{>", "}$NORMAL"));
+        bash_parser.insert(Repo::Behind, ("${LIGHT_RED}{<", "}$NORMAL"));
+        bash_parser.insert(Repo::Unstaged, ("|${YELLOW}", "$NORMAL"));
+        bash_parser.insert(Repo::Staged, ("|${GREEN}", "$NORMAL"));
+
+        let mut zsh_parser = HashMap::new();
+        zsh_parser.insert(Repo::Branch, ("%F{cyan}", "%f"));
+        zsh_parser.insert(Repo::Ahead, ("%F{green}{>", "}%f"));
+        zsh_parser.insert(Repo::Behind, ("%F{red}{<", "}%f"));
+        zsh_parser.insert(Repo::Unstaged, ("|%F{yellow}", "%f"));
+        zsh_parser.insert(Repo::Staged, ("|%F{green}", "%f"));
+
+        let mut repo_parser = HashMap::new();
+        repo_parser.insert(String::from("bash"), bash_parser);
+        repo_parser.insert(String::from("zsh"), zsh_parser);
+
+        match repo_parser.get(shell) {
+            Some(parser) => {
+                let (branch_start, branch_end) = parser.get(&Repo::Branch).unwrap();
+                write(
+                    &mut branch_final,
+                    format_args!(
+                        "{}",
+                        branch_start.to_owned().to_owned() + branch + branch_end.to_owned()
+                    ),
+                ).expect("Error");
                 if ahead_count.len() > 0 {
-                    branch_final = format!("{}%F{{green}}{{>{}}}%f", branch_final, ahead_count);
+                    let (ahead_start, ahead_end) = parser.get(&Repo::Ahead).unwrap();
+
+                    write(
+                        &mut branch_final,
+                        format_args!(
+                            "{}",
+                            ahead_start.to_owned().to_owned() + ahead_count + ahead_end.to_owned()
+                        ),
+                    ).expect("Error");
                 }
                 if behind_count.len() > 0 {
-                    branch_final = format!("{}%F{{red}}{{<{}}}%f", branch_final, behind_count);
+                    let (behind_start, behind_end) = parser.get(&Repo::Behind).unwrap();
+
+                    write(
+                        &mut branch_final,
+                        format_args!(
+                            "{}",
+                            behind_start.to_owned().to_owned()
+                                + behind_count
+                                + behind_end.to_owned()
+                        ),
+                    ).expect("Error");
                 }
                 if unstaged_counts.len() > 0 {
-                    branch_final = format!("{}|%F{{yellow}}{}%f", branch_final, unstaged_counts)
+                    let (unstaged_start, unstaged_end) = parser.get(&Repo::Unstaged).unwrap();
+
+                    write(
+                        &mut branch_final,
+                        format_args!(
+                            "{}",
+                            unstaged_start.to_owned().to_owned()
+                                + &unstaged_counts
+                                + unstaged_end.to_owned()
+                        ),
+                    ).expect("Error");
                 }
                 if staged_counts.len() > 0 {
-                    branch_final = format!("{}|%F{{green}}{}%f", branch_final, staged_counts)
-                }
-            }
-            "bash" => {
-                branch_final = format!("${{CYAN}}{}", branch_final);
-                if ahead_count.len() > 0 {
-                    branch_final = format!("{}${{LIGHT_GREEN}}{{>{}}}", branch_final, ahead_count);
-                }
-                if behind_count.len() > 0 {
-                    branch_final = format!("{}${{LIGHT_RED}}{{<{}}}", branch_final, behind_count);
-                }
-                if unstaged_counts.len() > 0 {
-                    branch_final = format!("{}|${{YELLOW}}{}", branch_final, unstaged_counts)
-                }
-                if staged_counts.len() > 0 {
-                    branch_final = format!("{}|${{GREEN}}{}", branch_final, staged_counts)
+                    let (staged_start, staged_end) = parser.get(&Repo::Unstaged).unwrap();
+
+                    write(
+                        &mut branch_final,
+                        format_args!(
+                            "{}",
+                            staged_start.to_owned().to_owned()
+                                + &staged_counts
+                                + staged_end.to_owned()
+                        ),
+                    ).expect("Error");
                 }
             }
             _ => {}
