6f419db915fcc5df2a0d6217ade35f2355afc73f
Crandel <cradlemann@gmail.com>
2018-11-24 22:44:32 +0100

    Change escaping in normal color

--- src/bash.rs
+++ src/bash.rs
@@ -10,23 +10,23 @@ impl BashFormatter {
         let shell = ShellFormatter {
             branch: Wrapper {
                 start: String::from("${CYAN}"),
-                end: String::from("$NORMAL"),
+                end: String::from("${NORMAL}"),
             },
             ahead: Wrapper {
                 start: String::from("${LIGHT_GREEN}{>"),
-                end: String::from("}$NORMAL"),
+                end: String::from("}${NORMAL}"),
             },
             behind: Wrapper {
                 start: String::from("${LIGHT_RED}{<"),
-                end: String::from("}$NORMAL"),
+                end: String::from("}${NORMAL}"),
             },
             unstaged: Wrapper {
                 start: String::from("|${YELLOW}"),
-                end: String::from("$NORMAL"),
+                end: String::from("${NORMAL}"),
             },
             staged: Wrapper {
                 start: String::from("|${GREEN}"),
-                end: String::from("$NORMAL"),
+                end: String::from("${NORMAL}"),
             },
             modified_char: String::from("%"),
             deleted_char: String::from("-"),
